c542ee75ba531913086b7343de9daa0b2ac8424a
jose-duque <duquejo01@gmail.com>
2023-06-01 17:57:47 -0500

    :rocket: Github pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://duquejo01.github.io',
 	integrations: [
 		tailwind({
 			config: { applyBaseStyles: false },
